96b26639f50c35bb58d7a84f8ecebf2ac873f922
shanjairaj7 <shanjairajvlogs@gmail.com>
2021-08-21 15:14:13 +0530

    queries.js updated to only take in  as an input

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,13 +2,8 @@ import { gql } from "@apollo/client";
 
 // Query to get all the 50 popular repos
 export const POPULAR_REPOS = gql`
-  query PopularJavascriptRepos($first: Int!, $last: Int) {
-    search(
-      query: "language:JavaScript"
-      type: REPOSITORY
-      first: $first
-      last: $last
-    ) {
+  query PopularJavascriptRepos($first: Int!) {
+    search(query: "language:JavaScript", type: REPOSITORY, first: $first) {
       repositoryCount
       edges {
         node {
